f2273d9a0034ac445b4e1ca61f6513e76915f2fd
Wessias <viggowessner@gmail.com>
2020-08-27 11:01:55 +0200

    Brip brap uppgift klar

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -6,9 +6,21 @@ namespace Tasks
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your age ");
+            Console.WriteLine("Enter your name hemmis");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter your age ");
             var age = Console.ReadLine();
-            Console.WriteLine("Your age is " + age);
+
+            Console.WriteLine("Are you alive or dead? But really though.");
+            var aliveOrDead = Console.ReadLine();
+
+            Console.WriteLine("What's your favorite nummer?");
+            int favoriteNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your name is " + name + ", you're " + age + " years old, and you're " + aliveOrDead + ". And my favorite number is " + Math.Pow(favoriteNum, 2) + " which happens " +
+                "to be your favorite number raised to the power of 2.");
+            
         }
     }
 }
