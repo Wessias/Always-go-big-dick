9a4ea052ca8582e39efcf331e6a0abf11d7f521e
Wessias <viggowessner@gmail.com>
2020-08-27 10:38:33 +0200

    I DID THE TASK, ARE YOU PROUD OF ME?????

---
+++ Tasks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Your age is " + age);
+        }
+    }
+}
