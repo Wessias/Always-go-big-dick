7fc82df43703e759365d776ce3f1a5c9446cf61c
Wessias <viggowessner@gmail.com>
2020-09-08 13:10:34 +0200

    I did the thing

---
+++ Metoder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(3, 5));
+            
+        }
+
+
+        private static int Add(int num1, int num2)
+        {
+            
+            var sum = num1 + num2;
+
+            return sum;
+        }
+        
+
+        }
+    }    
