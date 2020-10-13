647a4a07af9fb4c0c8cf3d79130aba4eced2289d
Wessias <viggowessner@gmail.com>
2020-10-13 12:15:29 +0200

    Polymorphism

--- OOP/Product.cs
+++ OOP/Product.cs
@@ -7,11 +7,35 @@ namespace OOP
     class Product
     {
 
-        public string sc_product;
-
-
-
-
-
+        public virtual void ProductName()
+        {
+            Console.WriteLine("This is a product");
+        }
     }
-}
+
+    class MythicCarry : Product
+    {
+            public override void ProductName()
+            {
+                Console.WriteLine("Mythic carry");
+          }
+        }
+
+
+    class BoostTo3k : Product
+    {
+        public override void ProductName()
+        {
+            Console.WriteLine("Boost to 3k rating");
+        }
+    }
+
+
+    class ERPinGoldshire : Product
+    {
+        public override void ProductName()
+        {
+            Console.WriteLine("ERP in Goldshire");
+        }
+    }
+    }
