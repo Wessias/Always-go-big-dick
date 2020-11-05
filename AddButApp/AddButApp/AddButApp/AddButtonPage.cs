c50e1e915698b4374b10f69d89c52434687739ce
Wessias <viggowessner@gmail.com>
2020-11-05 11:51:10 +0100

    wtf am i doing

---
+++ AddButApp/AddButApp/AddButApp/AddButtonPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AddButApp
+{
+	public class AddButtonPage : ContentPage
+	{
+		public AddButtonPage ()
+		{
+            Grid grid = new Grid();
+            Title = "Label Grid Demo - C#";
+            int columnsNum = 6;
+            int rowsNum = 6;
+
+            for (int x = 0; x < columnsNum; x++)
+            {
+                for (int y = 0; y < rowsNum; y++)
+                {
+                    // Insert button into grid  
+                    Button b = new Button() { HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.Fill ,Text = x + ":" +y };
+                    grid.Children.Add(b, x, y);
+                }
+            }
+            Content = grid;
+        }
+	}
+}
---
+++ Calc/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class AddButtonPage : ContentPage
+    {
+        public AddButtonPage()
+        {
+            Grid grid = new Grid();
+            Title = "Label Grid Demo - C#";
+
+            int columnsNum = 6;
+            int rowsNum = 6;
+
+            for (int x = 0; x < columnsNum; x++)
+            {
+                for (int y = 0; y < rowsNum; y++)
+                {
+                    // Insert button into grid  
+                    Button b = new Button() { HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.Fill, Text = x + ":" + y };
+                }
+            }
+            Content = grid;
+        }
+    }
+
+
+
+
+}
