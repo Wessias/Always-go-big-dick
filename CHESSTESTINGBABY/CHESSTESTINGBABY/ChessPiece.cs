55f7cd355b9d54f8f545ff32fc83b2c8db999c76
Wessias <viggowessner@gmail.com>
2021-04-29 10:51:06 +0200

    CHESS

---
+++ CHESSTESTINGBABY/CHESSTESTINGBABY/ChessPiece.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace CHESSTESTINGBABY
+{
+    class ChessPiece : ViewModelBase
+    {
+        private Point _Pos;
+        public Point Pos
+        {
+            get { return this._Pos; }
+            set { this._Pos = value; RaisePropertyChanged(() => this.Pos); }
+        }
+
+        private PieceType _Type;
+        public PieceType Type
+        {
+            get { return this._Type; }
+            set { this._Type = value; RaisePropertyChanged(() => this.Type); }
+        }
+
+        private Player _Player;
+        public Player Player
+        {
+            get { return this._Player; }
+            set { this._Player = value; RaisePropertyChanged(() => this.Player); }
+        }
+    }
+
+
+
+    public enum Player
+    {
+        White,
+        Black
+    }
+
+    public enum PieceType
+    {
+        Pawn,
+        Rook,
+        Knight,
+        Bishop,
+        Queen,
+        King
+
+    }
+
+}
